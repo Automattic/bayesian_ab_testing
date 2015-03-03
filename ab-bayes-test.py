6c8bb369f0dc22e69ab0586a144654d67f89ed3a
Greg Ichneumon Brown <gbrown5878@gmail.com>
2015-03-03 00:00:55 -0700

    Bayes AB Testing: add WP.com homepage test; remove dead code; add timing

--- ab-bayes-test.py
+++ ab-bayes-test.py
@@ -10,6 +10,7 @@ from random import random, normalvariate
 from numpy import *
 from datetime import *
 import os
+import time
 
 # Input data
 alpha = 1
@@ -62,10 +63,10 @@ low_threshold = 0.05
 low_threshold_suffix = ' reached 5%'
 
 #WP.com Homepage Signup
-#chart_name = 'WP.com Homepage Signup Simulation  (xxx users per hour)'
-#prior_conv_rate = 0.35
-#samples_per_hour = 100
-#days = 7
+chart_name = 'WP.com Homepage Signup Simulation  (xxx users per hour)'
+prior_conv_rate = 0.1
+samples_per_hour = 12500
+days = 3
 
 #Akismet Plugin Signup : https://mc.a8c.com/tracks/akismet/acquisition/plugin-signup/
 #chart_name = 'Akismet Plugin Signup Simulation  (175 users per hour)'
@@ -74,10 +75,10 @@ low_threshold_suffix = ' reached 5%'
 #days = 7
 
 #Akismet Developer Signup
-chart_name = 'Akismet Developer Signup Simulation (3 users per hour)'
-prior_conv_rate = 13/255
-samples_per_hour = int( round( 255 / 4 / 24 ) )
-days = 21
+#chart_name = 'Akismet Developer Signup Simulation (3 users per hour)'
+#prior_conv_rate = 13/255
+#samples_per_hour = int( round( 255 / 4 / 24 ) )
+#days = 21
 
 #Build an audience distribution - assume start on Wed cause I like to deploy on Wed
 #hour_dist = [.01] * 24 #first day get 1% of audience per hour
@@ -112,11 +113,15 @@ a_mean = prior_conv_rate
 start_dt = datetime( 2015, 2, 25, 12) #Wed at noon GMT
 hour_dt = timedelta( hours = 1 )
 #b_improvements = linspace( -0.1, 0.1, 21)
-b_improvements = [-0.15, -0.1, -0.05, 0.0, 0.05, 0.1, 0.15]
+# eleven 5% improvements are just as good as three 20% improvements
+#b_improvements = [-0.2, -0.15, -0.1, -0.05, 0.0, 0.05, 0.1, 0.15, 0.2]
+b_improvements = [-0.2, 0.0, 0.2]
 results_bayes_uniform = []
 results_bayes_linear = []
 results_bayes_audience = []
 results_isvalid = []
+sim_time = time.clock()
+worst_model_time = 0
 for i in b_improvements:
     b_mean = a_mean * (1.0 + i)
     samples_a = []
@@ -167,7 +172,11 @@ for i in b_improvements:
 	#alpha = int(audience_percent * num_samples) + 1
 	alpha = int( ( audience_percent * num_samples ) * prior_conv_rate ) + 1
 	beta = int( alpha * (1/prior_conv_rate - 1) ) + 1
+	model_time = time.clock()
 	p = prob_b_beats_a( sample_cnt, conv_a_cnt, sample_cnt, conv_b_cnt, alpha, beta )
+	model_time = time.clock() - model_time
+	if ( model_time > worst_model_time ):
+	    worst_model_time = model_time
 	bayes_series_audience.append( p )
 	#print curr_dt.strftime( "%a %H: " ) + "N: " + str(sample_cnt) + " s_a: " + str(conv_a_cnt) + " s_b: " + str(conv_b_cnt) + " alpha: " + str(alpha) + " beta: " + str(beta) + " aud: " + str(audience_percent) + " prob: " + str(p)
 
@@ -178,6 +187,11 @@ for i in b_improvements:
     results_bayes_linear.append(bayes_series_linear)
     results_bayes_audience.append(bayes_series_audience)
     results_isvalid.append(isvalid_series)
+
+sim_time = time.clock() - sim_time
+
+print "Total Simulation Time: " + str(sim_time)
+print "Worst Model Time: " + str(worst_model_time)
 
 hourly_sample_cnt = []
 series_time = []
@@ -263,7 +277,7 @@ for sn in range(1,4):
     elif ( sn == 3 ):
 	data = results_bayes_audience
 	thresh = thresh_audience
-	splt.set_title('Bayesian Probability B beats A (prior based on percentage of weekly audience)')
+	splt.set_title('Bayesian Probability B beats A (prior logarithmically decreases over one week)')
 
     splt.set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, len(b_improvements))])
     for i in range( 0, len(b_improvements) ):
@@ -284,56 +298,5 @@ for sn in range(1,4):
 	splt.annotate(thresh[k][1], xy=(thresh[k][0], hgt), xytext=(thresh[k][0] + 12*hour_dt, hgt), arrowprops=dict(facecolor='black', shrink=0.05) )
 	hgt += 0.2
 
-
-#Uniform
-#ax[1].set_title('Bayesian Probability B beats A (uniform prior)')
-#ax[1].set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, len(b_improvements))])
-#for i in range( 0, len(b_improvements) ):
-#    ax[1].plot( series_time, results_bayes_uniform[i] )
-#
-#ax[1].axhline(0.95, color='k', linestyle='-.')
-#ax[1].axhline(0.99, color='k', linestyle='--')
-#ax[1].axhline(0.05, color='k', linestyle='-.')
-#ax[1].axhline(0.01, color='k', linestyle='--')
-#ax[1].xaxis.set_major_locator(days_loc)
-#ax[1].xaxis.set_major_formatter(daysFmt)
-#ax[1].xaxis.set_minor_locator(hours_loc)
-#ax[1].set_ylim([0,1.0])
-#
-#hgt = 0.1
-#for k in thresh_uniform.keys():
-#    ax[1].axvline(thresh_uniform[k][0],color='k')
-#    ax[1].annotate(thresh_uniform[k][1], xy=(thresh_uniform[k][0], hgt), xytext=(thresh_uniform[k][0] + 6*hour_dt, hgt), arrowprops=dict(facecolor='black', shrink=0.05) )
-#    hgt += 0.2
-#
-##Linear
-#ax[2].set_title('Bayesian Probability B beats A (prior linearly decreases over one week)')
-#ax[2].set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, len(b_improvements))])
-#for i in range( 0, len(b_improvements) ):
-#    ax[2].plot( series_time, results_bayes_linear[i] )
-#
-#ax[2].axhline(0.95, color='k', linestyle='-.')
-#ax[2].axhline(0.99, color='k', linestyle='--')
-#ax[2].axhline(0.05, color='k', linestyle='-.')
-#ax[2].axhline(0.01, color='k', linestyle='--')
-#ax[2].xaxis.set_major_locator(days_loc)
-#ax[2].xaxis.set_major_formatter(daysFmt)
-#ax[2].xaxis.set_minor_locator(hours_loc)
-#ax[2].set_ylim([0,1.0])
-#
-##Audience
-#ax[3].set_title('Bayesian Probability B beats A (prior based on percentage of weekly audience)')
-#ax[3].set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, len(b_improvements))])
-#for i in range( 0, len(b_improvements) ):
-#    ax[3].plot( series_time, results_bayes_linear[i] )
-#
-#ax[3].axhline(0.95, color='k', linestyle='-.')
-#ax[3].axhline(0.99, color='k', linestyle='--')
-#ax[3].axhline(0.05, color='k', linestyle='-.')
-#ax[3].axhline(0.01, color='k', linestyle='--')
-#ax[3].xaxis.set_major_locator(days_loc)
-#ax[3].xaxis.set_major_formatter(daysFmt)
-#ax[3].xaxis.set_minor_locator(hours_loc)
-#ax[3].set_ylim([0,1.0])
 
 plt.show()
