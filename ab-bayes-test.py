f3c33f87521e4d25d475a92887a93d001dd1ff65
Greg Ichneumon Brown <gbrown5878@gmail.com>
2015-05-12 16:45:52 -0600

    Final testing - add variation of simulated conversion rate based on weekday/weekend - remove the linear changing of the prior simulation - switch to a constant prior - simpler - adjust thresholds to 99%

--- ab-bayes-test.py
+++ ab-bayes-test.py
@@ -15,23 +15,32 @@ import time
 #################################
 # Input data for simulations
 
+#Percent improvements of B conv rate over A conv
+# eleven 5% improvements are just as good as three 20% improvements
+
 #WP.com Homepage Signup
 chart_name = 'WP.com Homepage Signup Simulation  (12.5k users per hour)'
 prior_conv_rate = 0.1
+weekend_var = 0.02 # +/- 2% for weekends/weekdays
 samples_per_hour = 12500
-days = 7
+days = 7 #21
+b_improvements = [-0.05, -0.02, -0.01, 0.0, 0.01, 0.02, 0.05]
 
 #Akismet Plugin Signup
 #chart_name = 'Akismet Plugin Signup Simulation  (175 users per hour)'
 #prior_conv_rate = 0.1456
+#weekend_var = 0.12	 # +/- 2% for weekends/weekdays
 #samples_per_hour = 175
-#days = 7
+#days = 7 #21
+b_improvements = [-0.2, -0.15, -0.1, -0.05, -0.02, 0.0, 0.02, 0.05, 0.1, 0.15, 0.2]
 
 #Akismet Developer Signup
 #chart_name = 'Akismet Developer Signup Simulation (3 users per hour)'
 #prior_conv_rate = 13/255
+#weekend_var = 0.05 # +/- 2% for weekends/weekdays
 #samples_per_hour = int( round( 255 / 4 / 24 ) )
 #days = 21
+b_improvements = [-0.2, -0.15, -0.1, -0.05, -0.02, 0.0, 0.02, 0.05, 0.1, 0.15, 0.2]
 
 #################################
 # Configuration
@@ -39,19 +48,15 @@ days = 7
 significant_cutoff = 1000
 significant_cutoff_text = '1,000 samples'
 
-hi_threshold = 0.95
-hi_threshold_suffix = ' sim at 95%'
-low_threshold = 0.05
-low_threshold_suffix = ' sim at 5%'
+hi_threshold = 0.99 #0.95
+hi_threshold_suffix = ' sim at 99%'
+low_threshold = 0.01 #0.05
+low_threshold_suffix = ' sim at 1%'
 
 # simulate all cases where the test reduces/improves the conversion rate from -5% to +5%
 start_dt = datetime( 2015, 2, 25, 12) #Wed at noon GMT
 hour_dt = timedelta( hours = 1 )
 min_wait_dt = timedelta( days = 5 )
-
-#Percent improvements of B over A for testing
-# eleven 5% improvements are just as good as three 20% improvements
-b_improvements = [-0.2, -0.15, -0.1, -0.05, -0.01, 0.0, 0.01, 0.05, 0.1, 0.15, 0.2]
 
 #################################
 # Model Parameters
@@ -77,7 +82,9 @@ a = .010227857
 b = 97.772192161
 hour_dist = []
 last = 0
-for i in range(0,7*24):
+step = 1 #does not work right yet
+samples_per_step = step * samples_per_hour
+for i in range(0,7*24,step):
     n = - abs( log(a*(i+b)) )
     hour_dist.append( last - n )
     last = n
@@ -119,8 +126,13 @@ def isvalid_b_beats_a( N_a, s_a, N_b, s_b ):
 #################################
 # Simulations
 
-num_samples = days * 24 * samples_per_hour
-a_mean = prior_conv_rate
+num_samples = days * 24 * samples_per_step
+a_mean_weekday = prior_conv_rate * (1 + 2*weekend_var)
+a_mean_weekend = prior_conv_rate * (1 - 5*weekend_var)
+
+print a_mean_weekday
+print a_mean_weekend
+print '---------------------'
 
 results_bayes_uniform = []
 results_bayes_linear = []
@@ -129,11 +141,23 @@ results_isvalid = []
 sim_time = time.clock()
 worst_model_time = 0
 for i in b_improvements:
-    b_mean = a_mean * (1.0 + i)
-    samples_a = np.random.random(num_samples)
-    samples_b = np.random.random(num_samples)
-    samples_a = np.int8(samples_a < a_mean)
-    samples_b = np.int8(samples_b < b_mean)
+    samples_a = []
+    samples_b = []
+    b_mean_weekday = a_mean_weekday * (1.0 + i)
+    b_mean_weekend = a_mean_weekend * (1.0 + i)
+    print b_mean_weekday
+    print b_mean_weekend
+    tmp_a = np.random.random(num_samples)
+    tmp_b = np.random.random(num_samples)
+    curr_dt = start_dt
+    for j in xrange( 0, num_samples):
+	if ( curr_dt.weekday() in set([5,6]) ):
+	    samples_a.append( int(tmp_a[j] < a_mean_weekend) )
+	    samples_b.append( int(tmp_b[j] < b_mean_weekend) )
+	else:
+	    samples_a.append( int(tmp_a[j] < a_mean_weekday) )
+	    samples_b.append( int(tmp_b[j] < b_mean_weekday) )
+	curr_dt += hour_dt * step
 
     #look at the samples in chunks of one hour
     bayes_series_linear = []
@@ -143,8 +167,8 @@ for i in b_improvements:
     curr_dt = start_dt
     audience_percent = 0.0
     aud_idx = 0
-    for j in xrange( 0, num_samples, samples_per_hour):
-	sample_cnt = j + samples_per_hour
+    for j in xrange( 0, num_samples, samples_per_step):
+	sample_cnt = j + samples_per_step
 	conv_a_cnt = np.sum( samples_a[0:sample_cnt] )
 	conv_b_cnt = np.sum( samples_b[0:sample_cnt] )
 	if ( aud_idx < len(hour_dist) - 1 ):
@@ -162,15 +186,19 @@ for i in b_improvements:
 	# alpha = number of successful conversions
 	# beta = number of failed conversions
 	#alpha = num_samples - j + 1
-	alpha = int( (num_samples - j) * prior_conv_rate ) + 1
-	beta = int( alpha * (1/prior_conv_rate - 1) ) + 1
-	bayes_series_linear.append( prob_b_beats_a( sample_cnt, conv_a_cnt, sample_cnt, conv_b_cnt, alpha, beta ) )
+	#alpha = int( (num_samples - j) * prior_conv_rate ) + 1
+	#beta = int( alpha * (1/prior_conv_rate - 1) ) + 1
+	#bayes_series_linear.append( prob_b_beats_a( sample_cnt, conv_a_cnt, sample_cnt, conv_b_cnt, alpha, beta ) )
 
 	#audience
 	# balance with total weekly audience not yet seen
-	#alpha = int(audience_percent * num_samples) + 1
-	alpha = int( ( audience_percent * num_samples ) * prior_conv_rate ) + 1
+	#alpha = int( ( audience_percent * num_samples ) * prior_conv_rate ) + 1
+	#beta = int( alpha * (1/prior_conv_rate - 1) ) + 1
+
+	#Prior of half a week's average conversion rate
+	alpha = int( num_samples/2 * prior_conv_rate );
 	beta = int( alpha * (1/prior_conv_rate - 1) ) + 1
+
 	model_time = time.clock()
 	p = prob_b_beats_a( sample_cnt, conv_a_cnt, sample_cnt, conv_b_cnt, alpha, beta )
 	model_time = time.clock() - model_time
@@ -180,10 +208,10 @@ for i in b_improvements:
 	#print curr_dt.strftime( "%a %H: " ) + "N: " + str(sample_cnt) + " s_a: " + str(conv_a_cnt) + " s_b: " + str(conv_b_cnt) + " alpha: " + str(alpha) + " beta: " + str(beta) + " aud: " + str(audience_percent) + " prob: " + str(p)
 
 	isvalid_series.append( isvalid_b_beats_a( sample_cnt, conv_a_cnt, sample_cnt, conv_b_cnt ) )
-	curr_dt += hour_dt
+	curr_dt += hour_dt * step
 
     results_bayes_uniform.append(bayes_series_uniform)
-    results_bayes_linear.append(bayes_series_linear)
+    #results_bayes_linear.append(bayes_series_linear)
     results_bayes_audience.append(bayes_series_audience)
     results_isvalid.append(isvalid_series)
 
@@ -203,8 +231,8 @@ idx = 0
 thresh_uniform = {}
 thresh_linear = {}
 thresh_audience = {}
-for j in range( 0, num_samples, samples_per_hour):
-    hourly_sample_cnt.append( j + samples_per_hour )
+for j in range( 0, num_samples, samples_per_step):
+    hourly_sample_cnt.append( j + samples_per_step )
     if ( ( cutoff_date == 0 ) and ( j > significant_cutoff ) ):
 	cutoff_date = curr_dt
     series_time.append( curr_dt )
@@ -212,7 +240,7 @@ for j in range( 0, num_samples, samples_per_hour):
     #Generate threshold crossings
     # Must have a minimum number of hours (6 right now) before we call something in case there are no conversions
     test_idx = 0
-    if ( idx > 6 ):
+    if ( idx > 24 ):
 	for i in b_improvements:
 	    if ( i > 0 ):
 		prefix = '+'
@@ -224,17 +252,17 @@ for j in range( 0, num_samples, samples_per_hour):
 		thresh_uniform[i] = (curr_dt,name + hi_threshold_suffix)
 	    if ( not thresh_uniform.has_key(i) and ( results_bayes_uniform[test_idx][idx] <= low_threshold ) ):
 		thresh_uniform[i] = (curr_dt,name + low_threshold_suffix)
-	    if ( not thresh_linear.has_key(i) and ( results_bayes_linear[test_idx][idx] >= hi_threshold ) ):
-		thresh_linear[i] = (curr_dt,name + hi_threshold_suffix)
-	    if ( not thresh_linear.has_key(i) and ( results_bayes_linear[test_idx][idx] <= low_threshold ) ):
-		thresh_linear[i] = (curr_dt,name + low_threshold_suffix)
+#	    if ( not thresh_linear.has_key(i) and ( results_bayes_linear[test_idx][idx] >= hi_threshold ) ):
+#		thresh_linear[i] = (curr_dt,name + hi_threshold_suffix)
+#	    if ( not thresh_linear.has_key(i) and ( results_bayes_linear[test_idx][idx] <= low_threshold ) ):
+#		thresh_linear[i] = (curr_dt,name + low_threshold_suffix)
 	    if ( not thresh_audience.has_key(i) and ( results_bayes_audience[test_idx][idx] >= hi_threshold ) ):
 		thresh_audience[i] = (curr_dt,name + hi_threshold_suffix)
 	    if ( not thresh_audience.has_key(i) and ( results_bayes_audience[test_idx][idx] <= low_threshold ) ):
 		thresh_audience[i] = (curr_dt,name + low_threshold_suffix)
 	    test_idx += 1
 
-    curr_dt += hour_dt
+    curr_dt += hour_dt * step
     idx += 1
 
 #################################
@@ -249,7 +277,7 @@ daysFmt = mdates.DateFormatter('%a') #day of week
 
 # Colormaps: http://matplotlib.org/1.2.1/examples/pylab_examples/show_colormaps.html
 colormap = plt.cm.winter
-fig, ax = plt.subplots(4, 1)
+fig, ax = plt.subplots(3, 1)
 
 #IsValid
 ax[0].set_title('IsValid Probability B beats A', fontsize=12)
@@ -283,24 +311,24 @@ for i in b_improvements:
 	prefix = '+'
     else:
 	prefix = '-'
-    line_names.append( prefix + str(abs(i * 100)) + '%' )
+    line_names.append( prefix + str(abs(i * 100)) + '% : ' + str(round(prior_conv_rate*(1+i)*100,1)) )
 fig.legend( tuple(lines), tuple(line_names), loc='center right', title='% improvement of B over A' )
 fig.suptitle( chart_name, fontsize=14 )
 
-for sn in range(1,4):
+for sn in range(1,3):
     splt = ax[sn]
     if ( sn == 1 ):
 	data = results_bayes_uniform
 	thresh = thresh_uniform
 	splt.set_title('Bayesian Probability B beats A (uniform prior)', fontsize=12)
+#    elif ( sn == 2 ):
+#	data = results_bayes_linear
+#	thresh = thresh_linear
+#	splt.set_title('Bayesian Probability B beats A (prior linearly decreases over one week)', fontsize=12)
     elif ( sn == 2 ):
-	data = results_bayes_linear
-	thresh = thresh_linear
-	splt.set_title('Bayesian Probability B beats A (prior linearly decreases over one week)', fontsize=12)
-    elif ( sn == 3 ):
 	data = results_bayes_audience
 	thresh = thresh_audience
-	splt.set_title('Bayesian Probability B beats A (prior logarithmically decreases over one week)', fontsize=12)
+	splt.set_title('Bayesian Probability B beats A (prior is ave for one half week)', fontsize=12)
 
     splt.set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, len(b_improvements))])
     for i in range( 0, len(b_improvements) ):
